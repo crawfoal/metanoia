0cb6d68e63c7bb6f2efb94ca46a4fc0d916c26c2
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-04-11 16:30:48 -0700

    Address some comments from Hound and add configuration for Sass stuff

--- spec/features/support/show_flash_with.rb
+++ spec/features/support/show_flash_with.rb
@@ -1,8 +1,8 @@
 RSpec::Matchers.define :show_flash_with do |expected_message, expected_style = nil|
   flash_selector =
     case expected_style
-      when nil then 'div[class^="flash"]'
-      else "div.flash-#{expected_style}"
+    when nil then 'div[class^="flash"]'
+    else "div.flash-#{expected_style}"
     end
 
   match do |actual|
