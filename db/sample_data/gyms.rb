84a99b1b35ff3cf340ac1e75af648c37a34f003f
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-04-11 00:44:22 -0700

    Create a rake task to populate the database

---
+++ db/sample_data/gyms.rb
@@ -0,0 +1,66 @@
+FactoryGirl.define do
+  factory :wild_walls, parent: :gym do
+    name 'Wild Walls'
+    transient do
+      section_names [
+        'The 45',
+        'The Vert',
+        'The Overhang',
+        'The Back Wall',
+        'The Slab',
+        'Opposite Back Wall',
+        'The Cave'
+      ]
+    end
+  end
+
+  factory :boulders_climbing_gym, parent: :gym do
+    name 'Boulders Climbing Gym'
+    transient do
+      section_names [
+        'The 45',
+        'The Vert',
+        'The Overhang',
+        'The Back Wall',
+        'The Slab',
+        'Opposite Back Wall',
+        'The Cave'
+      ]
+    end
+  end
+
+  factory :movement_boulder, parent: :gym do
+    name 'Movement - Boulder'
+    transient do
+      section_names [
+        'Front Corner',
+        'The 45',
+        'East Vert',
+        'The Cave',
+        'Back Corner',
+        'West Vert'
+      ]
+    end
+  end
+
+  factory :the_spot, parent: :gym do
+    name 'The Spot'
+    transient do
+      section_names [
+        'Left River',
+        'Right River',
+        'Font',
+        'Left Dojo',
+        'Right Dojo',
+        'Back Scoop of the Hueco',
+        'Back Right Hueco',
+        'Big Front Scoop of the Hueco',
+        'Small Scoop of the Hueco',
+        'Left Beach',
+        'Middle Meach',
+        'Right Beach',
+        'Yosemite'
+      ]
+    end
+  end
+end
--- spec/factories/gyms.rb
+++ spec/factories/gyms.rb
@@ -1,5 +1,15 @@
 FactoryGirl.define do
   factory :gym do
+    transient do
+      section_names []
+    end
+
+    after :build do |gym, evaluator|
+      evaluator.section_names.each do |section_name|
+        gym.sections << build(:section, name: section_name)
+      end
+    end
+
     trait :with_name do
       name "Gym Name"
     end
