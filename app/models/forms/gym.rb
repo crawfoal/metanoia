a24bc75c95b346577b95bb0ebd3dedc71448b633
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-04-11 13:34:44 -0700

    Add a missing space in the error message

--- app/models/forms/gym.rb
+++ app/models/forms/gym.rb
@@ -38,7 +38,7 @@ module Forms
 
     def gym_cannot_be_blank
       unless @gym.value(reject_blanks: true).present?
-        errors.add(:base, 'Cannot create a empty record for a gym.' \
+        errors.add(:base, 'Cannot create a empty record for a gym. ' \
                           'You must define some information for the gym itself, '\
                           'or create some sections for the gym.')
       end
