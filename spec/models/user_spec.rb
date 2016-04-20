c085fdede0d037132ec25fd5a81b4a01e1a253ad
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-04-20 16:30:59 -0700

    Install and setup Rolify

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#add_role' do
+    it 'assigns the role to the user (smoke test for Rolify)' do
+      user = build :user
+      user.add_role :admin
+      expect(user).to have_role :admin
+    end
+  end
 end
