effead28b631514117ee2b19a79dddb78ded1e1e
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-04-19 18:13:50 -0700

    Add in necessary devise routes, style devise views and fix navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   root 'home#show'
 
-  devise_for :users, skip: :all
+  devise_for :users, only: :passwords
   devise_scope :user do
     post 'sign_up', to: 'devise/registrations#create', as: :user_registration
     post 'sign_in', to: 'devise/sessions#create', as: :user_session
     delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
   end
-  get '/', to: 'home#show', as: :new_user_session
+  get 'sign_up', to: 'devise/registrations#new', as: :new_user_registration
+  get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
 
   resources :gyms, except: :destroy
   resources :sections, only: [:new, :show]
