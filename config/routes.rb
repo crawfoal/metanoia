6e73f738b4b86525ff4bbf0251e377b7e00f27c6
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-04-20 21:56:22 -0700

    Allow users to change account information
    - Rewrote route definitions for Devise to make it easier to work with. The only route we really want to skip is the one for destroying user registrations (at least for now...).
    - Refactored the `DeviseFeatureHelper` a little.
    - Added a `Profile` controller and view to distinguish between updating general user information, and updating account information (which is controlled by Devise).
    - Made a custom view for editing user registration because we aren't initially implementing account deletion (and I wanted to change the header).

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,20 @@
 Rails.application.routes.draw do
   root 'home#show'
 
-  devise_for :users, only: :passwords
+  devise_for :users, skip: :registrations
   devise_scope :user do
-    post 'sign_up', to: 'devise/registrations#create', as: :user_registration
-    post 'sign_in', to: 'devise/sessions#create', as: :user_session
-    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+    resource :registration,
+      only: [:new, :create, :edit, :update],
+      path: 'users',
+      path_names: { new: 'sign_up' },
+      controller: 'devise/registrations',
+      as: :user_registration do
+        get :cancel
+      end
   end
-  get 'sign_up', to: 'devise/registrations#new', as: :new_user_registration
-  get 'sign_in', to: 'devise/sessions#new', as: :new_user_session
 
   resources :gyms, except: :destroy
   resources :sections, only: [:new, :show]
   resources :climbs, only: [:new, :create]
+  resource :profile, only: [:show], controller: :profile
 end
--- spec/features/home_spec.rb
+++ spec/features/home_spec.rb
@@ -12,8 +12,7 @@ RSpec.feature 'Homepage', type: :feature, js: true do
     visit root_path
     expect(page).to_not have_selector '#sign_out'
 
-    click_on 'Sign In'
-    login_user
+    create_and_login_user
     expect(page).to be_user_default_page
     expect(page).to have_selector '#sign_out'
 
@@ -28,9 +27,7 @@ RSpec.feature 'Homepage', type: :feature, js: true do
   scenario 'signed-in user clicks on logo' do
     skip
 
-    visit root_path
-    click_on 'Sign In'
-    login_user
+    create_and_login_user
 
     find('.logo').click
     expect(page).to be_user_default_page
