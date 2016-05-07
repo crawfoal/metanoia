16c350e2bcc2a42dac7bf67ba32090863a3f3987
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-05-06 22:04:20 -0700

    Address one last comment from Hound

--- app/models/climb_log.rb
+++ app/models/climb_log.rb
@@ -3,6 +3,8 @@ class ClimbLog < ActiveRecord::Base
   validates_presence_of :athlete_story
   belongs_to :climb
   validates_presence_of :climb
-  scope :in_section, ->(section) { joins(:climb).where(climbs: { section_id: section.id }) }
+  scope :in_section, ->(section) do
+    joins(:climb).where(climbs: { section_id: section.id })
+  end
   delegate :grade, :color, to: :climb
 end
