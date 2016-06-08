30fe818afbd5fc03b9ec9370649bf176a91d1a10
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-06-07 21:16:44 -0500

    Don't show route histogram if the gym doesn't have any routes

--- spec/displays/route_histogram_spec.rb
+++ spec/displays/route_histogram_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe RouteHistogram do
     it 'returns an array of grade name and route count pairs' do
       trg = create :tiny_route_gym
 
-      rh = RouteHistogram.new(Climb.where(type: 'Route'))
+      rh = RouteHistogram.new(trg.routes)
       rh_data = rh.data
 
       expect(rh_data.size).to be > 0
@@ -14,6 +14,20 @@ RSpec.describe RouteHistogram do
         expect(grade).to be_present
         expect(Climb.where(grade_id: grade.id).count).to eq num_climbs
       end
+    end
+  end
+
+  describe '#has_data?' do
+    it 'returns a falsey value if there are no routes in the given relation' do
+      tbg = create :tiny_boulder_gym
+      rh = RouteHistogram.new(tbg.routes)
+      expect(rh.has_data?).to be_falsey
+    end
+
+    it 'returns a truthy value if there are routes in the given relation' do
+      trg = create :tiny_route_gym
+      rh = RouteHistogram.new(trg.routes)
+      expect(rh.has_data?).to be_truthy
     end
   end
 end
--- spec/views/gyms/show.html.haml_spec.rb
+++ spec/views/gyms/show.html.haml_spec.rb
@@ -11,4 +11,16 @@ RSpec.describe 'gyms/show.html.haml', type: :view do
       expect(rendered).to_not have_selector '.edit-gym-link'
     end
   end
+
+  context "when the gym doesn't have any routes" do
+    it "doesn't include the .route-histogram div" do
+      athlete = create :athlete
+      allow(view).to receive(:current_user).and_return(athlete)
+      gym = create :gym
+      assign(:gym, gym)
+      assign(:route_histogram, RouteHistogram.new(gym.routes))
+      render
+      expect(rendered).to_not have_selector '.route-histogram'
+    end
+  end
 end
