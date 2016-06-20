b862ecd1cc1314e5127d4774fa0c1fb6b4c8733d
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-06-20 18:04:28 -0500

    Use the TableDependencyGraph in SeedMigrations

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,6 @@
 require 'active_record/fixtures'
 
-ActiveRecord::Base.transaction do
-  ActiveRecord::Base.connection.execute("SET CONSTRAINTS ALL DEFERRED")
-  ActiveRecord::FixtureSet.create_fixtures(
-    SeedMigrations.migrator.fixture_directory,
-    SeedMigrations.configuration.seeded_tables
-  )
-end
+ActiveRecord::FixtureSet.create_fixtures(
+  SeedMigrations.migrator.fixture_directory,
+  SeedMigrations.configuration.seeded_tables
+)
