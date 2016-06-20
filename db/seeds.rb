9229c804c09ecfbd83196c63b7c0af579088d7c6
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-06-20 16:22:50 -0500

    Try wrapping the fixture building in a transaction
    I think that this might solve the issue with fixture fk dependencies because the default for Postgres is to make constraints deferrable to the end of the transaction.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,8 @@
 require 'active_record/fixtures'
 
-ActiveRecord::FixtureSet.create_fixtures(
-  SeedMigrations.migrator.fixture_directory,
-  SeedMigrations.configuration.seeded_tables
-)
+ActiveRecord::Base.transaction do
+  ActiveRecord::FixtureSet.create_fixtures(
+    SeedMigrations.migrator.fixture_directory,
+    SeedMigrations.configuration.seeded_tables
+  )
+end
--- spec/support/helpers/capture_output.rb
+++ spec/support/helpers/capture_output.rb
@@ -1,10 +1,11 @@
 module CaptureOutput
   def capture_stdeo
-    original_stderr, original_stdout = $stderr, $stdout
+    original_stderr, original_stdout = $stderr.dup, $stdout.dup
     log_file = File.new("#{Rails.root}/spec/console.log", 'w')
     $stderr = log_file
     $stdout = log_file
     yield
+  ensure
     $stderr, $stdout = original_stderr, original_stdout
   end
 end
