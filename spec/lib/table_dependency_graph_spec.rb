230f691d9959d9786a8ea5cbaeee5a9e709af828
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-06-21 06:31:19 -0500

    Address a few comments from Hound

--- spec/lib/table_dependency_graph_spec.rb
+++ spec/lib/table_dependency_graph_spec.rb
@@ -3,7 +3,8 @@ require "#{Rails.root}/lib/table_dependency_graph"
 
 RSpec.describe TableDependencyGraph do
   describe '#tsort' do
-    it 'returns a topological sort of the tables, where the dependency is the fk constraint' do
+    it 'returns a topological sort of the tables, where the dependency is the '\
+       'fk constraint' do
       tdg = TableDependencyGraph.new(:grade_systems, :grades, :buckets)
       expect(tdg.tsort.last).to eq 'grades'
     end
--- spec/tasks/seed/seed_migrations_spec.rb
+++ spec/tasks/seed/seed_migrations_spec.rb
@@ -5,7 +5,9 @@ RSpec.describe SeedMigrations do
   include SeedMigrationsHelper
 
   describe '.configuration.seeded_tables' do
-    it 'returns the tables, ordered by dependencies (i.e. if table A has a fk pointing to table B, then table B will come before table A in the list)' do
+    it 'returns the tables, ordered by dependencies (i.e. if table A has a fk '\
+       'pointing to table B, then table B will come before table A in the '\
+       'list)' do
       SeedMigrations.configure do |config|
         config.seeded_tables = [:grade_systems, :grades, :buckets]
       end
