d7e5b13fdfec11248a90d834d6f9cbdd94dceae6
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-07-19 12:09:44 -0500

    Only show link to employee list if they're allowed to view it

--- spec/views/gyms/index.html.haml_spec.rb
+++ spec/views/gyms/index.html.haml_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
+RSpec::Matchers.define :link_to_employee_list_for do |gym|
+  match do |page|
+    matched_row = page.find '#gyms tr', text: /.*#{gym.name}.*/
+    matched_row.has_link? 'Employee List'
+  end
+
+  failure_message do |page|
+    "expected \"#{page.native}\" to have link for #{gym.name}'s employee list"
+  end
+
+  failure_message_when_negated do |page|
+    "expected \"#{page.native}\" to not have link for #{gym.name}'s employee list"
+  end
+end
+
 RSpec.describe 'gyms/index.html.haml', type: :view do
   context "when the user's current role isn't admin" do
     it "doesn't show the link to create a new gym" do
@@ -11,6 +26,22 @@ RSpec.describe 'gyms/index.html.haml', type: :view do
       render
 
       expect(rendered).to_not have_selector '#new_gym'
+    end
+  end
+
+  context 'when the current user is a manager' do
+    it "only links to the employee list for the gym the user works at" do
+      employer_gym = create :gym, name: 'Employer Gym'
+      other_gym = create :gym, name: 'Other Gym'
+      user = create :manager, employed_at: employer_gym
+      allow(view).to receive(:current_user).and_return(user)
+      assign(:gyms, [employer_gym, other_gym])
+
+      render
+      page = Capybara.string(rendered)
+
+      expect(page).to link_to_employee_list_for employer_gym
+      expect(page).to_not link_to_employee_list_for other_gym
     end
   end
 end
