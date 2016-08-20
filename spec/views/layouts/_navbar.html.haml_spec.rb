9f090981fdb8fa49c042c75f493e7cbbdd19d59e
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-08-20 09:45:29 -0500

    Show link depending on current role, not all roles

--- spec/views/layouts/_navbar.html.haml_spec.rb
+++ spec/views/layouts/_navbar.html.haml_spec.rb
@@ -1,8 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe 'layouts/_navbar.html.haml', type: :view do
-  it "doesn't show the 'My Log' link when the current user isn't an athlete" do
-    allow(controller).to receive(:current_user).and_return(create(:visitor))
+  it "doesn't show the 'My Log' link when the current user's current role "\
+     "isn't an athlete" do
+    user = create :setter
+    user.add_role :athlete
+    allow(controller).to receive(:current_user).and_return(user)
 
     render
 
