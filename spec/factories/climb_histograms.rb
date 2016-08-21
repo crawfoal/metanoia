ffefd71b4bca43a43322be4434c7994b7084cad6
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-08-21 11:23:01 -0500

    Don't use let, create factory for histograms

---
+++ spec/factories/climb_histograms.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :climb_histogram do
+    initialize_with { new(climbs, grade_system) }
+    skip_create
+
+    transient do
+      gym_factory :gym
+      gym { create gym_factory }
+      climb_type { 'boulder' }
+      climbs { gym.send(climb_type.downcase.pluralize) }
+      grade_system { gym.send("#{climb_type.downcase}_grade_system") }
+    end
+  end
+end
