02450053de636929a995f39e4db4a879c3e0539f
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-08-21 12:04:57 -0500

    Clean up

--- spec/forms/base_form_spec.rb
+++ spec/forms/base_form_spec.rb
@@ -16,30 +16,44 @@ class SomeForm < BaseForm
 end
 
 RSpec.describe BaseForm do
-  let(:form) { SomeForm.new }
+  def build_spy_form
+    form = SomeForm.new
+    allow(form).to receive(:persist!)
+    form
+  end
 
   describe '#save' do
     it 'calls `persist!` if the record is valid' do
+      form = build_spy_form
       allow(form).to receive(:valid?).and_return(true)
-      expect(form).to receive(:persist!)
+
       form.save
+
+      expect(form).to have_received(:persist!)
     end
 
     it 'returns a falsey value if the record is not valid' do
+      form = SomeForm.new
+
       allow(form).to receive(:valid?).and_return(false)
+
       expect(form.save).to be_falsey
     end
   end
 
   describe '#save!' do
     it 'calls `persist!` if the record is valid' do
+      form = build_spy_form
       allow(form).to receive(:valid?).and_return(true)
-      expect(form).to receive(:persist!)
+
       form.save!
+      expect(form).to have_received(:persist!)
     end
 
     it 'raises an error if the record is not valid' do
+      form = SomeForm.new
       allow(form).to receive(:valid?).and_return(false)
+      
       expect { form.save! }.to \
         raise_error(/errors:\n\tmessage 1\n\tmessage 2\n/)
     end
