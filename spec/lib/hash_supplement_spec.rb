35e5e55b0656f5edf9a6fdeb47377b04dd81637d
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-08-23 09:51:42 -0500

    Fix spacing

--- spec/lib/hash_supplement_spec.rb
+++ spec/lib/hash_supplement_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe HashSupplement do
   describe '#first_key' do
     it 'returns the first key of the hash' do
       h = { a: 1, b: 2, c: 3 }
+      
       expect(h.first_key).to eq :a
     end
   end
