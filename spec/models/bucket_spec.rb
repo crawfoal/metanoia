b6e2aa54bf2c54ceb8f2af003cb39ce85a7eaf1a
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-08-23 11:00:50 -0500

    Write a spec for `.ordered`

--- spec/models/bucket_spec.rb
+++ spec/models/bucket_spec.rb
@@ -3,4 +3,17 @@ require 'rails_helper'
 RSpec.describe Bucket, type: :model do
   it { should belong_to :grade_system }
   it { should have_many(:grades) }
+
+  describe '.ordered' do
+    it "should order the buckets according the contained grades' "\
+       "`sequence_number`" do
+      hueco_buckets = Bucket.where(
+        grade_system: GradeSystem.find_by_name('Hueco')
+      )
+      
+      expect(hueco_buckets.ordered.map(&:name)).to eq \
+        ["V1 & ↓", "V2 - V3", "V4 - V5", "V6 - V7", "V8 - V9", "V10 - V11",
+         "V12 & ↑"]
+    end
+  end
 end
