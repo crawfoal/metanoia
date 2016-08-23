5664243adcde71ed4697f672c95895e1cb523faf
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-08-23 09:42:03 -0500

    Add more of an explanation

--- spec/lib/active_record_supplement_spec.rb
+++ spec/lib/active_record_supplement_spec.rb
@@ -3,8 +3,10 @@ require 'rails_helper'
 RSpec.describe ActiveRecordSupplement do
   describe '#pluck' do
     it 'returns an array of the requested attributes values, nested in another'\
-       ' array' do
+       ' array (because this is how an AR collection with one element would '\
+       'behave)' do
       climb = build :climb, color: 1, type: 'Route'
+      
       expect(climb.pluck(:color, :type)).to eq [[1, 'Route']]
     end
   end
