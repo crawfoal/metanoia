b7da4329458bee068ba000f01b0086df0f08f978
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-08-23 11:12:10 -0500

    Clean up

--- spec/models/climb_log_spec.rb
+++ spec/models/climb_log_spec.rb
@@ -7,16 +7,16 @@ RSpec.describe ClimbLog, type: :model do
   it { should validate_presence_of :climb  }
   it { should respond_to :color }
   it { should respond_to :grade }
+  it { should respond_to :color_name }
 
   describe '#in_section' do
     it 'should return all climb logs in the given section' do
-      climb1 = create :climb
-      section = climb1.section
-      climb2 = create :climb, section: section
-
+      section = create :section
       athlete = create :athlete
-      ClimbLogger.new({ climb_id: climb1.id }, athlete).log
-      ClimbLogger.new({ climb_id: climb2.id }, athlete).log
+      create_list :climb_log,
+                  2,
+                  climb: create(:climb, section: section),
+                  athlete_story: athlete.athlete_story
 
       expect(athlete.athlete_story.climb_logs.in_section(section).size).to eq 2
     end
