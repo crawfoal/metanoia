9b7e2cd7f2d72b12c79485712dddf5c1b3dda8d6
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-08-23 09:28:26 -0500

    Add a test that describe how `render_as_local` works

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,9 +2,22 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
   describe '#render_as_local' do
+    it "is a shortcut to calling render when the name of the local variable "\
+       "should be the same as the instance variable's name" do
+      employment_form = create(:employment_form)
+      assign(:employment_form, employment_form)
+
+      expect(helper.render_as_local(:employment_form)).to eq \
+        helper.render(
+          partial: 'employments/form',
+          locals: { employment_form:  employment_form}
+        ) # in the view/controller,     ^this is actually @employment_form
+    end
+
     it "raises an error when the specified instance variable doesn't implement"\
        ' `to_partial_path`' do
       assign(:string, "I don't implement the correct interface")
+
       expect { helper.render_as_local :string }.to \
         raise_error(/doesn't implement `to_partial_path`/)
     end
