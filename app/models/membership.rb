a6c1809fcd5a73e44e2f1addc066b205737b6ed5
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-08-23 13:39:48 -0500

    Add note for the future

--- app/models/membership.rb
+++ app/models/membership.rb
@@ -1,4 +1,10 @@
 class Membership < ActiveRecord::Base
   belongs_to :athlete_story
   belongs_to :gym
+  # This table is intended to have a "classification" column (or something like
+  # that) at some point, that would indicate if the athlete has a monthly
+  # membership, punch card, is just a day pass user, etc. It isn't intended to
+  # represent only users who have an actual membership in the sense that gyms
+  # use this term, but rather it is just saying that a user has been to the gym
+  # before.
 end
