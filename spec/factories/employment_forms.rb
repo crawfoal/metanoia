12f6ba222d387ff61c59f73a9e27e1e4c4c1b89f
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-08-30 13:25:10 -0500

    Clean up `employment_form` factory, and don't use a `user` transient attribute`
    It's more clear to just specify things that are specified when building a form.

--- spec/factories/employment_forms.rb
+++ spec/factories/employment_forms.rb
@@ -3,17 +3,14 @@ FactoryGirl.define do
     initialize_with { new(attributes.merge(gym_id: gym.id)) }
 
     transient do
-      user { create :setter }
       gym { create :gym }
     end
 
-    email { user.email }
-    role_name { user.current_role }
+    email { create(:user).email }
+    role_name { Employment.roles.sample.to_s }
 
     trait :with_unregistered_email do
-      user nil
       email 'fake_address@example.com'
-      role_name { Employment.roles.sample.to_s }
     end
   end
 end
