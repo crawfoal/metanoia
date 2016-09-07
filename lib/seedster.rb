85dbc6dd3c7025c0044954636d601c1d286a6cc4
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-09-07 15:27:11 -0500

    Try patching AR's disable ri
    Hopefully this will allow us to load seeds in on Heroku. It wraps the whole seed loading in a transaction and sets all constraints to deferred so that they aren't checked until the end of the transaction (i.e. after all seeds are loaded/reloaded).

--- lib/seedster.rb
+++ lib/seedster.rb
@@ -5,6 +5,7 @@ require "#{Rails.root}/lib/table_dependency_graph"
 require "#{Rails.root}/lib/fixturizer"
 require 'active_record/fixtures'
 require_relative 'seedster/active_record_helpers'
+require_relative 'seedster/referential_integrity_patch'
 
 module Seedster
   class << self
@@ -24,7 +25,17 @@ module Seedster
     end
 
     def fill_tables
-      ActiveRecord::FixtureSet.create_fixtures(fixture_directory, tables)
+      # waiting on Rails PR #21233 - then can go back to simple one liner that
+      # is in the else block
+      # (see Seedster::ReferentialIntegrityPatch for transaction wrapper method)
+      if ActiveRecord::Base.connection.instance_of? \
+         ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+        ActiveRecord::Base.connection.wrap_with_transaction_that_disables_ri do
+          ActiveRecord::FixtureSet.create_fixtures(fixture_directory, tables)
+        end
+      else
+        ActiveRecord::FixtureSet.create_fixtures(fixture_directory, tables)
+      end
     end
 
     def migrate
---
+++ lib/seedster/referential_integrity_patch.rb
@@ -0,0 +1,44 @@
+# delete once Rails PR #21233 is merged in and we've upgraded
+module Seedster
+  module ReferentialIntegrityPatch
+    def wrap_with_transaction_that_disables_ri
+      disable_all_user_triggers
+      transaction do
+        execute("SET CONSTRAINTS ALL DEFERRED")
+        yield
+      end
+      enable_all_user_triggers
+    end
+
+    def disable_all_user_triggers
+      statements = ActiveRecord::Base.connection.tables.collect do |name|
+        "ALTER TABLE #{quote_table_name(name)} DISABLE TRIGGER USER"
+      end
+      execute(statements.join(";"))
+    end
+
+    def enable_all_user_triggers
+      statements = ActiveRecord::Base.connection.tables.collect do |name|
+        "ALTER TABLE #{quote_table_name(name)} ENABLE TRIGGER USER"
+      end
+      execute(statements.join(";"))
+    end
+
+    def self.monkey_patches?(klass)
+      instance_methods(false).any? do |method_name|
+        klass.respond_to? method_name
+      end
+    end
+  end
+end
+
+class ActiveRecord::ConnectionAdapters::PostgreSQLAdapter
+  if Seedster::ReferentialIntegrityPatch.monkey_patches?(self)
+    raise 'Whoops, `ActiveRecord::ConnectionAdapters::PostgreSQLAdapter` '\
+          'now implments one of the methods in '\
+          "`Seedster::ReferentialIntegrityPatch`, so we can't include that "\
+          'module in the class'
+  else
+    include Seedster::ReferentialIntegrityPatch
+  end
+end
