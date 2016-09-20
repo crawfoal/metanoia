78fb684e15eb7242921bca7e8d42210468bf43dc
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-09-20 08:00:32 -0500

    Specify enum value rather than id...
    That spec should have never been passing, Rails just fixed something.

--- spec/lib/active_record_supplement_spec.rb
+++ spec/lib/active_record_supplement_spec.rb
@@ -5,9 +5,9 @@ RSpec.describe ActiveRecordSupplement do
     it 'returns an array of the requested attributes values, nested in another'\
        ' array (because this is how an AR collection with one element would '\
        'behave)' do
-      climb = build :climb, color: 1, type: 'Route'
+      climb = build :climb, color: '#b71c1c', type: 'Route'
 
-      expect(climb.pluck(:color, :type)).to eq [[1, 'Route']]
+      expect(climb.pluck(:color, :type)).to eq [['#b71c1c', 'Route']]
     end
   end
 end
