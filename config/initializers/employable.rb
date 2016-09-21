0134f1e97f6ac15717233a37eb016a47f5224444
Amanda Dolan <amandacrawfordalc@gmail.com>
2016-09-21 12:37:19 -0500

    Use new `to_prepare` method

--- config/initializers/employable.rb
+++ config/initializers/employable.rb
@@ -1,6 +1,6 @@
 unless Rails.application.config.cache_classes &&
        Rails.application.config.eager_load
-  ActionDispatch::Reloader.to_prepare do
+  ActiveSupport::Reloader.to_prepare do
     Dir["#{Rails.root}/app/models/*_story.rb"].each do |file|
       require_dependency file
     end
